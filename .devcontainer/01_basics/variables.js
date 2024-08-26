c2c11f20c3e7756cfc99f9025a4b2cf19983a563
sourav5-dev <souravkaushik5@gmail.com>
2024-08-26 20:07:48 +0000

    conversion of datatypes

---
+++ .devcontainer/01_basics/variables.js
@@ -0,0 +1,8 @@
+const accountId = 14453
+let accountEmail = "sourav@google.com"
+var accountPassword = "@12345"
+accountcity = "Noida"
+let accountState;
+
+
+console.table([accountId,accountEmail,accountPassword,accountcity,accountState])
