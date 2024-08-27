6e7a1b1fdda3b4fdcc78d4b8721527d2485903b8
sourav5-dev <souravkaushik5@gmail.com>
2024-08-27 16:03:29 +0000

    math&num

---
+++ .devcontainer/01_basics/06_nums_and_maths.js
@@ -0,0 +1,37 @@
+const score = 400
+
+const balance = new Number(100)
+// console.log(balance);
+// console.log(score);
+
+// console.log(balance.toString().length);
+
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8637;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000
+
+// console.log(hundreds.toLocaleString("en-IN"));
+// // +++++++++++ Maths +++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(4));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.2)); //for top value
+// console.log(Math.floor(4.9)); //for lower value
+console.log(Math.random(2,3,5,8,91,0,10));
+console.log(Math.random()); //it give us the value between 0-1
+
+console.log((Math.random()*10) + 1);
+console.log((Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+console.log(Math.floor(Math.random()*(max-min+1))+ min);
+
+
+
