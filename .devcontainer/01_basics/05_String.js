74ae75ae91af0d7137b86c021db5234a9e5ce256
sourav5-dev <souravkaushik5@gmail.com>
2024-08-27 14:46:13 +0000

    Strings

---
+++ .devcontainer/01_basics/05_String.js
@@ -0,0 +1,21 @@
+const name ="Sourabh"
+const repoCount = 30
+
+//console.log(name + repoCount)
+
+// console.log('Hello My Name is ${name} + and my Repo count is {repoCount}');
+
+const gameName = new String("ravikant-hc-com")
+console.log(gameName[0])
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf("i"));
+
+const url = "https://sourabh.com/sourabh%20Kaushik"
+
+console.log(url.replace("%20", "_"));
+
+console.log (url.includes("sourabh"))
+console.log(gameName.split("-"));
