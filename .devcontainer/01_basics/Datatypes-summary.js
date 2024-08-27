3364a71e5be37a050556e04d0810de82f755d4f3
sourav5-dev <souravkaushik5@gmail.com>
2024-08-27 13:29:28 +0000

    DataTypes in JavaScript

---
+++ .devcontainer/01_basics/Datatypes-summary.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types : String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+ 
+const id = Symbol('123')
+
+const anotherId = Symbol ('123')
+
+// console.log(id == anotherId)
+
+// Reference (Non-Primitive)
+
+// Array, Objects, Functions
+
+
+const myFunction = function(){
+    console.log("Hello World");
+}
+
+
+
